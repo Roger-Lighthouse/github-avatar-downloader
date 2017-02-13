76d95878db9c7c051834c7be833987cb872143bc
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-13 21:46:59 +0000

    add function getRepoContributors

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,13 @@
 var request=require("request");
 
 console.log('Hello World');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  cb('oh', 'shit');
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
