e7c93957d735b5ae7221a4961375f96606158e9e
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-13 21:57:52 +0000

    created url to get avatars'

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,13 @@
 var request=require("request");
+var GITHUB_USER='Roger-Lighthouse';
+var GITHUB_TOKEN='e1f218d5dececc03bc8566f31b07ddf2a14afee1';
+
 
 console.log('Hello World');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  p(requestURL);
   cb('oh', 'shit');
 }
 
@@ -10,4 +15,11 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
+
+
+function p(item){
+  console.log(item);
+}
 
