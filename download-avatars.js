ef6f02dd54ffd9363dc23a3f92fdc0d7cbe3fb8d
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-14 01:34:59 +0000

    Added command line args and throws error if fails

--- download-avatars.js
+++ download-avatars.js
@@ -4,30 +4,47 @@ var fs=require('fs');
 
 var GITHUB_USER='Roger-Lighthouse';
 var GITHUB_TOKEN='e1f218d5dececc03bc8566f31b07ddf2a14afee1';
+var repoOwner=process.argv[2];
+var repoName=process.argv[3];
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  p(requestURL);
+  if(repoName!=null && repoOwner!=null){
+    var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    p(requestURL);
 
-  var options = {
-    url: requestURL,
-    headers: {"User-Agent": "Student Project"}
+    var options = {
+      url: requestURL,
+      headers: {"User-Agent": "Student Project"}
+    }
+    request.get(options, cb);  // Note 1
+  }else{
+    console.log("Fuck You, No Params");
   }
-  request(options, cb);               // Note 1
-
 }
 
 
-getRepoContributors("jquery", "jquery", function (err, result, body) {
+getRepoContributors(repoOwner, repoName, function (err, result, body) {
   //console.log("Errors:", err);
  // console.log("Result:", body);
   var objectArray=JSON.parse(body);
   for(var i = 0; i < objectArray.length; i++){
     p(objectArray[i].avatar_url);
+    downloadImageByURL(objectArray[i].avatar_url, './avatars/'+objectArray[i].login+'.jpg');
   }
 
 });
+
+function downloadImageByURL(url, path) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err  ;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(path));               // Note 4
+}
 
 
 
