c4d0c6165df07e4687419cce7d28e6c85875694c
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-14 01:44:08 +0000

    Final copy of project

--- download-avatars.js
+++ download-avatars.js
@@ -9,10 +9,11 @@ var repoName=process.argv[3];
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if(repoName!=null && repoOwner!=null){
+  if(repoName != null && repoOwner != null){
     var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
     p(requestURL);
 
+    //set up an options object because I required a User-Agent
     var options = {
       url: requestURL,
       headers: {"User-Agent": "Student Project"}
@@ -23,10 +24,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 }
 
-
+// Invokes the getRepoContributors function
 getRepoContributors(repoOwner, repoName, function (err, result, body) {
-  //console.log("Errors:", err);
- // console.log("Result:", body);
   var objectArray=JSON.parse(body);
   for(var i = 0; i < objectArray.length; i++){
     p(objectArray[i].avatar_url);
@@ -35,6 +34,8 @@ getRepoContributors(repoOwner, repoName, function (err, result, body) {
 
 });
 
+
+// Called by getRepoContributors and downloads images into another folder, avatars
 function downloadImageByURL(url, path) {
   request.get(url)               // Note 1
        .on('error', function (err) {                                   // Note 2
@@ -48,7 +49,7 @@ function downloadImageByURL(url, path) {
 
 
 
-
+// This is my mini function to do console.log
 function p(item){
   console.log(item);
 }
