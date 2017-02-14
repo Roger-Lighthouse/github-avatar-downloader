074be380d14da80c78a6e1a82d2058653477ec30
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-14 00:02:03 +0000

    got urls using JSON

--- download-avatars.js
+++ download-avatars.js
@@ -1,23 +1,32 @@
 var request=require("request");
+var fs=require('fs');
+
+
 var GITHUB_USER='Roger-Lighthouse';
 var GITHUB_TOKEN='e1f218d5dececc03bc8566f31b07ddf2a14afee1';
 
 
-console.log('Hello World');
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   p(requestURL);
-  request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-    console.log('Response Status Code:', response.statusCode);
-    cb(err, body);
-  });
+
+  var options = {
+    url: requestURL,
+    headers: {"User-Agent": "Student Project"}
+  }
+  request(options, cb);               // Note 1
+
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+
+getRepoContributors("jquery", "jquery", function (err, result, body) {
+  //console.log("Errors:", err);
+ // console.log("Result:", body);
+  var objectArray=JSON.parse(body);
+  for(var i = 0; i < objectArray.length; i++){
+    p(objectArray[i].avatar_url);
+  }
+
 });
 
 
